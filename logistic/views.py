84912602d424cbbf6adbf6a380af2ecdaa59fe97
Lyotaaa <korystin.aleksei@yandex.ru>
2023-08-24 19:57:29 +0300

    readd files

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,7 @@
+from rest_framework.decorators import api_view
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -22,3 +24,8 @@ class StockViewSet(ModelViewSet):
     ordering_fields = ["id", "address"]
     filterset_fields = ["products"]
     search_fields = ["products__title", "products__description"]
+
+
+@api_view(["GET"])
+def text_view(request):
+    return Response("Проверка")
